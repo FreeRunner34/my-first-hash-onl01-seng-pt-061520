37bec88712ee5012c8589afa10e90c9c564d9435
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-06-27 06:01:50 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+hash = {"names" => "Nikolaus", "names" => "Brianna", "age" => 18, "age" => 21}
 end
 
 
